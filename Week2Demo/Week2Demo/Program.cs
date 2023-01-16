9550f59a6b636125c99d2b902ba61d21157ca160
RaniaArbash <41278913+RaniaArbash@users.noreply.github.com>
2023-01-16 11:33:37 -0500

    Add files via upload

---
+++ Week2Demo/Week2Demo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Week2Demo
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    // This code is completed by Rania
+        //    double n1;
+        //    double n2;
+        //    double n3;
+        //    double sum;
+        //    double avg;
+
+        //    // double n1, n2, n3, sum, avg;
+        //    Console.WriteLine("This is a program to calcualte the average of three numbers ");
+
+        //    Console.WriteLine("Enter 3 numbers, one at a time ");
+
+        //    Console.WriteLine("Enter First Number ");
+        //    n1 = Convert.ToDouble(Console.ReadLine());
+
+        //    Console.WriteLine("Enter Second Number ");
+        //    n2 = Convert.ToDouble(Console.ReadLine());
+
+        //    Console.WriteLine("Enter Third Number ");
+        //    n3 = Convert.ToDouble(Console.ReadLine());
+
+        //    sum = n1 + n2 + n3;
+        //    avg = sum / 3;
+
+        //    Console.WriteLine("The average of the three numbers " + n1 + ", " + n2 + " ," + n3 + " is " + avg);
+
+        //}
+    }
+}
---
+++ Week2Demo/Week2Demo/SecondProblem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Week2Demo
+{
+    public class SecondProblem
+    {
+        static void Main(string[] args)
+        {
+
+            double s1;
+            double s2;
+            double s3;
+            double s4;
+            double per;
+
+            //    // double n1, n2, n3, sum, avg;
+            Console.WriteLine("This is a program to calcualte the perimeter of a polygon ");
+
+            Console.WriteLine("Enter 4 numbers, the sides of the polygon");
+
+            Console.WriteLine("Enter First side ");
+            s1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second side ");
+            s2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter third side ");
+            s3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter forth side ");
+            s4 = Convert.ToDouble(Console.ReadLine());
+
+
+            per = s1 + s2 + s3 + s4;
+
+            Console.WriteLine("the polygon's perimeter is " + per);
+
+
+        }
+    }
+
+}
